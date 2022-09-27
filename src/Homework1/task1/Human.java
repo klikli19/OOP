93d8dd8f92d9e7d8dee06a3e98068ab3bacfc768
klikli19 <103356373+klikli19@users.noreply.github.com>
2022-09-27 19:22:54 +0300

    справила класс Human дз1

--- src/Homework1/task1/Human.java
+++ src/Homework1/task1/Human.java
@@ -3,14 +3,29 @@ package Homework1.task1;
 import java.time.LocalDate;
 
 public class Human {
-    String name;
-    int yearOfBirth;
-    String town;
+    private String name;
+    private int yearOfBirth;
+    private String town;
 
     public void setYearOfBirth(int age) {
         yearOfBirth = LocalDate.now().getYear() - age;
     }
 
+    public String getTown() {
+        return town;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public String getName() {
+        return name;
+    }
 
     public void greetings () {
         System.out.println("Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в " + yearOfBirth +
